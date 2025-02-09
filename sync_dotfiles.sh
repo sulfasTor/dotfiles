72747dfffab2d6245798c96faea315fb74df9489
sulfastor <torresmoisesa@gmail.com>
2025-02-08 19:42:01 -0600

    Fix sync_dotfiles.sh

--- sync_dotfiles.sh
+++ sync_dotfiles.sh
@@ -1,5 +1,5 @@
 # Syncs docsfiles
 mkdir -p ~/.emacs.d/
 
-ln -s emacs/init.el ~/.emacs.d/init.el
-ln -s emacs/custom.el ~/.emacs.d/custom.el
+ln -s $(pwd)/emacs/init.el ~/.emacs.d/init.el
+ln -s $(pwd)/emacs/custom.el ~/.emacs.d/custom.el
